53d9320f2043e3c3290e1f648ad8484fe6c3866a
kraiwith <kraiwithkamchu@gmail.com>
2020-04-22 16:09:40 +0700

    add angular universal, config build environment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
+import { environment } from './../environments/environment';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'dkww-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'dont-know-what-web';
+  environment = environment;
 }
