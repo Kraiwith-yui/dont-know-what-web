41a25204046c19f891a9c6cd59d252f7e28cb5ee
kraiwith <kraiwithkamchu@gmail.com>
2020-04-22 14:07:00 +0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dkww-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'dont-know-what-web';
+}
