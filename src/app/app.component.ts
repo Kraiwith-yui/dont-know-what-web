853c8118096d2c7d9cd3044f96039ed51a2defd1
kraiwith <kraiwithkamchu@gmail.com>
2020-04-24 09:55:55 +0700

    add fonts, add bootstrap and jquery, test bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,33 @@
 import { environment } from './../environments/environment';
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'dkww-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dont-know-what-web';
   environment = environment;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  isBrowser(): boolean {
+    return isPlatformBrowser(this.platformId);
+  }
+
+  ngOnInit() {
+    console.log(`#: AppComponent -> ngOnInit -> this.isBrowser()`, this.isBrowser());
+    if (this.isBrowser()) {
+      $(function () {
+        const popovers: any = $('[data-toggle="popover"]');
+        popovers.popover({ container: 'body', trigger: 'hover' });
+        const toasts: any = $('.toast');
+        toasts.toast('show');
+        const tooltips: any = $('[data-toggle="tooltip"]');
+        tooltips.tooltip();
+      });
+    }
+  }
 }
